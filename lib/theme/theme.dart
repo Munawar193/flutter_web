3f0d86a6be6b45134eddaffd5984123a9b660967
munawar <munawartmk@gmail.com>
2022-01-09 22:08:48 +0700

    intial commit

---
+++ lib/theme/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kBlackColor = const Color(0xff1D1E3C);
+Color kOrangeColor = const Color(0xffFE998D);
+Color kTransparentColor = Colors.transparent;
+
+TextStyle blackStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
